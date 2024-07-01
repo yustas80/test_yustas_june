3de890dcb8f535cac1bda0b5cbb947f8b88b5a4e
yustas80 <yustas80@gmail.com>
2024-07-01 16:18:20 +0300

    add lesson_3

---
+++ lesson_2/lesson_2.py
@@ -0,0 +1,14 @@
+# Task 7
+
+x = input("Please Enter 3-digits number: ")
+x = int(x)
+
+a = x // 100
+b = x // 10 % 10
+c = x % 10
+
+print(a)
+print(b)
+print(c)
+
+print('Have a Nice Day!')
---
+++ lesson_3/lesson_3.py
@@ -0,0 +1,13 @@
+# Task 6
+
+a = input('Вкажіть сумму у USD: ')
+
+b = 40.82
+
+print(f'Курс обміну USD - {b}')
+
+c = a * b
+
+print(f'Ви отримаєте {c} UAH')
+
+print('Have a Nice Day!')
